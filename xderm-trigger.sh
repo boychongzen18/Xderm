d6e5113e9f025fe4df23263982421d165a310460
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2021-06-06 23:00:02 +0700

    Update xderm-trigger.sh

--- xderm-trigger.sh
+++ xderm-trigger.sh
@@ -8,7 +8,7 @@ echo " ################################### "
 
 opkg update
 opkg install wget
-wget https://github.com/boychongzen18/Xderm/blob/main/xderm-trigger.ipk
+wget https://github.com/boychongzen18/Xderm/blob/2627dbe07776dbad20b11c9b3d07b0ebff0aa0ec/xderm-trigger_1.1.1-1_aarch64_cortex-a53.ipk
 opkg install xderm-trigger*.ipk
 
 
