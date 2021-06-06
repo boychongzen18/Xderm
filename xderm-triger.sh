1084ce41a0e1ac7385f4d90ab1bde1d281b7d4c6
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2021-06-06 16:31:53 +0700

    Update xderm-triger.sh

--- xderm-triger.sh
+++ xderm-triger.sh
@@ -7,6 +7,7 @@ echo "      Update & Installer Packet    "
 echo " ################################### "
 
 opkg update
+opkg install unzip
 wget https://github.com/boychongzen18/Xderm-Mini/blob/main/xderm-trigger-1.1.1.zip
 unzip xderm-trigger-1.1.1.zip
 rm xderm-trigger-1.1.1.zip
