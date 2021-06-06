4383d298f6fc5a042395a3d90334e34f11c8b53b
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2021-06-06 19:05:28 +0700

    Add files via upload

---
+++ xderm-triger.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+echo " ################################### "
+
+echo "      Update & Installer Packet    "
+
+echo " ################################### "
+
+opkg update
+opkg install wget
+wget https://github.com/boychongzen18/Xderm/blob/main/xderm-trigger.ipk
+opkg install xderm-trigger*.ipk
+
+
+echo " ################################### "
+
+echo "   Jangan Lupa Reboot STB Openwrt "
+
+echo " ################################### "
+
+echo "  Creator By Boychongzen aka Xroot  "
+
+echo " ################################### "
+################### "
